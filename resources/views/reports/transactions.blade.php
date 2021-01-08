8631b6bacbd25a7a16cadb16a38709878630accf
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2021-01-08 11:08:13 +0100

    fixed jquery error

--- resources/views/reports/transactions.blade.php
+++ resources/views/reports/transactions.blade.php
@@ -89,7 +89,8 @@
 {{-- {!! $dataTable->table(['width' => '100%']) !!} --}}
 
 @push('scripts_lib')
-  <script src="https://code.jquery.com/jquery-3.5.1.js" defer=""></script>
+  {{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
+  
   <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
