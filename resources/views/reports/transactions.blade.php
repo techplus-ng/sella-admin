f8bf2177881ef4f06ccaff7886d4a2305b620ee6
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2021-01-08 11:06:59 +0100

    fixed error with datatable export

--- resources/views/reports/transactions.blade.php
+++ resources/views/reports/transactions.blade.php
@@ -82,14 +82,33 @@
 @endsection
 
 @push('css_lib')
-@include('layouts.datatables_css')
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
 @endpush
 
 {{-- {!! $dataTable->table(['width' => '100%']) !!} --}}
 
 @push('scripts_lib')
-@include('layouts.datatables_js')
-{{-- {!! $dataTable->scripts() !!} --}}
+  <script src="https://code.jquery.com/jquery-3.5.1.js" defer=""></script>
+  <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#datatable').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+      });
+    });
+  </script>
 @endpush
 
 
