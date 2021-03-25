7a87e8a9cea55be72c59bda87dd357f841e26aa8
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2021-03-25 10:33:17 +0100

    fixed update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
         parent::boot();
     }
 
