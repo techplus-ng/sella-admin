b65fe0795df3eab7bdb004a572db4af43fe985ea
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2021-03-25 08:58:42 +0100

    fixed udpate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         try {
 
             config(['mail.driver' => setting('mail_driver', 'smtp')]);
