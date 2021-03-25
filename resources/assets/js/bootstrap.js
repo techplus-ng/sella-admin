15ae864516a13c41feb22dec9e195638d75d05e7
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2021-03-25 08:56:43 +0100

    fixed ajax

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -21,7 +21,7 @@ try {
  */
 
 window.axios = require('axios');
-
+window.axios.defaults.baseURL = 'https://getsella.com';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
